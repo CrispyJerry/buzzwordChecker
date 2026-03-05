3b5f77d37773843890db5d87c31971111a3cc574
German Migdalovich <germanmigd@gmail.com>
2026-03-05 13:13:58 +0000

    fixed save conflict, now all code for basic program is present

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from pypdf import PdfReader
+import re
+
+with open('buzzwords.txt', 'r') as scan:
+    BUZZWORDS = [line.strip() for line in scan]
+
+
+
 
 def pdfToText(pdf):
     reader = PdfReader(pdf)
-    
-    page = reader.pages[0]
-    
-    text = page.extract_text()
-    print(text)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
 
+def buzzwordChecker(pdf):
+    buzzwordCount = 0
+    for word in BUZZWORDS:
+        if re.search(fr"\b{word}\b", pdf, re.IGNORECASE):
+            buzzwordCount += 1
+            print(f"Found buzzword: {word}")
+    if buzzwordCount == 0:
+        print("No Buzzwords have been found in this CV")
 
-path = r'PDF-DirectoryPath'
-pdfToText(path)
+path = r'PDF-Path'
+text = pdfToText(path)
+buzzwordChecker(text)
